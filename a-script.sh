7cee135f08b836b7c5b39a3907f71895255d33fb
Yury Lunev <xalt@mail.ru>
2021-03-11 11:04:44 +0300

    Добавить ярлык на рабочий стол. Добавить названия Уровней конфиденциальности. Добавить README

--- a-script.sh
+++ a-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "Введите название папки:"
+echo "Введите название папки (будет помещена в /):"
 read folder
+folder='/'$folder
 echo "Введите имя пользователя администратора:"
 read admin
 echo "Введите пароль, который будет установлен ВСЕМ пользователям:"
@@ -9,25 +10,31 @@ echo "Сменить пароль пользователю при входе в 
 read chage
 share_group=share_users
 share_folder=share
-LEVELS_NAME=(НС ДСП С СС)
 
 groupadd $share_group
 
 groups_by_default=audio,cdrom,plugdev,scanner,video,share_users
 
-cp profile /etc/profile
-cp -R config/* /usr/share/fly-wm/config
-sed -i 's/$folder/file:\/\/\/${folder}/' /usr/share/fly-wm/config/libreoffice/4/user/registrymodifications.xcu
+cp conf/profile /etc/profile
+cp conf/mac_levels /etc/parsec/mac_levels
+cp -R conf/Desktops /usr/share/fly-wm/
+sed -i 's/$folder/\'${folder}'/' /usr/share/fly-wm/Desktops/Desktop1/docs.desktop
+cp -R conf/libreoffice /usr/share/fly-wm/config
+sed -i 's/$folder/file:\/\/\'${folder}'/' /usr/share/fly-wm/config/libreoffice/4/user/registrymodifications.xcu
 
-USERS=($(awk 'BEGIN{FS=", "} {print $1}' users))
-USERS_LEVEL=($(awk 'BEGIN{FS=", "} {print $2}' users))
+LEVELS_NAME=($(awk 'BEGIN{FS=":"} {print $1}' conf/mac_levels))
+MAC_LEVELS=($(awk 'BEGIN{FS=":"} {print $2}' conf/mac_levels))
+
+USERS=($(awk 'BEGIN{FS=", "} {print $1}' $1))
+USERS_LEVEL=($(awk 'BEGIN{FS=", "} {print $2}' $1))
 
 for user_id in ${!USERS[*]}
 do
 	username=${USERS[user_id]}
 	user_level=${USERS_LEVEL[user_id]}
 	useradd --groups $groups_by_default --shell /bin/bash -m $username
-	if ["$chage" = "y"]; then
+	if [[ $chage = 'y' ]]; then
+		echo 'change password'$username
 		chage --lastday 0 $username
 	fi
 	echo $username:$password | sudo chpasswd
@@ -40,9 +47,10 @@ chown $admin:$share_group $folder
 chmod u+rwx,g+rx,o-rwx $folder
 pdpl-file 3:0:0:ccnr $folder
 
-for level in ${!LEVELS_NAME[*]}
+for i in ${MAC_LEVELS[*]}
 do
-	path=$folder/${LEVELS_NAME[$level]}
+	level=${MAC_LEVELS[$i]}
+	path=$folder/${LEVELS_NAME[$i]}
 	mkdir $path
 	chown $admin:$share_group $path
 	chmod u+rwx,g+rx,o-rwx $path
@@ -64,8 +72,8 @@ do
 
 	path=$path/$share_folder
 	mkdir $path
-        chown -v $admin:$share_group $path
-        chmod u+rwx,g+rwxs,o-rwx $path
-        pdpl-file -v $level:0:0 $path
+	chown -v $admin:$share_group $path
+	chmod u+rwx,g+rwxs,o-rwx $path
+	pdpl-file -v $level:0:0 $path
 
 done
